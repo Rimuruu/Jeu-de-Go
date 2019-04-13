796a99d4a6c063b20041560c836e18ddd4fa8994
Rimuruu <antoine.renciot@gmail.com>
2019-04-13 12:49:08 +0200

    Update

--- Panel.java
+++ Panel.java
@@ -3,14 +3,21 @@ import java.awt.*;
 import java.io.*;
 import java.util.*;
 import javax.imageio.ImageIO;
+import java.awt.event.*;
 
 public class Panel extends JPanel{
 	private Image goban;
 	private Image goban2;
 	private Image goban3;
+	private Image pionnoir;
+	private Image pionblanc;
+
 	Panel(){
 		goban = Toolkit.getDefaultToolkit().getImage("goban19.png");
 		goban2 = Toolkit.getDefaultToolkit().getImage("goban9.png");
+		pionnoir = Toolkit.getDefaultToolkit().getImage("pionnoir9.png");
+		pionblanc = Toolkit.getDefaultToolkit().getImage("pionblanc9.png");
+	
 
 	}
 	@Override
@@ -19,5 +26,7 @@ public class Panel extends JPanel{
 		g.drawImage(goban,0,0,this);
 		
 		g.drawImage(goban2,0,0,this);
+		g.drawImage(pionnoir,70,70,this);
+		g.drawImage(pionblanc,162,70,this);
 	}
 }
